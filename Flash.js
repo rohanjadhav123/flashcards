4b024a8040c1633f16023b06e3aabebe285820d2
rohanjadhav123 <rohanjadhav123456@gmail.com>
2016-10-18 14:48:34 +0530

    Update Flash.js

--- Flash.js
+++ Flash.js
@@ -1,10 +1,6 @@
-$(document).ready(function(){
-		// set up hover panels
-		// although this can be done without JavaScript, we've attached these events
-		// because it causes the hover to be triggered when the element is tapped on a touch device
-		$('.hover').hover(function(){
-			$(this).addClass('flip');
-		},function(){
-			$(this).removeClass('flip');
-		});
-	});
+$('.flip').click(function(){
+        $(this).find('.card').addClass('flipped').mouseleave(function(){
+            $(this).removeClass('flipped');
+        });
+        return true;
+    });
